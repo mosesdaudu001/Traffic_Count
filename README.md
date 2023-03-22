7732258ee9f2db7d1779a14d122f9e242f8faf22
mosesdaudu001 <mosesdaudu001@gmail.com>
2023-03-22 14:51:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Detect Stopped vehicles In Video
+
+Hi my name is `Moses Daudu` and I am a Machine Learning Engineer.
+
+In this repo, You will see how to detect vehicles that stopped in a given video.
+
+## Step 1:
+Install all requirements
+`pip install -r requirements.txt`
+
+## Step 2:
+Edit the `FILE_PATH` argument in line 7 and input the path to the video you want to use
+
+## Step 3:
+Run the `python` file
+
+
+Contact Moses at `mosesdaudu001@gmail.com` for adjustments to suit your needs.
+
+### Side Notes:
+1. I used masks to improvve the efficiency of the detector. But as I'm sure that you are already aware that masks are specific to the video and so you would need your own customly made masks.
+2. Please note that I used the object path for my local computer, ensure to use yours to avoid problems
---
+++ traffic_count.py
@@ -0,0 +1,96 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import argparse
+from sort import *
+
+FILE_PATH = "C:/Users/itani/Downloads/roi/GitHub_Projects/Traffic_Count/Videos/10fps.mp4" 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+# For Mask
+mask = cv2.imread('C:/Users/itani/Downloads/roi/GitHub_Projects/Traffic_Count/masks/10dtp_1.png')
+
+# For Tracker
+tracker = Sort(max_age=200, min_hits=3, iou_threshold=0.3)
+cap  = cv2.VideoCapture(FILE_PATH)
+model = YOLO("C:/Users/itani/Downloads/roi/GitHub_Projects/Traffic_Count/yolo_weights/yolov8n.pt")
+
+# Initialize the coordinates for line which is specific for the video
+limitsUp = [834, 260, 837, 442]
+limitsDown = [335, 649, 592, 492]
+
+totalCountUp = []
+totalCountDown = []
+
+while True:
+    _, img = cap.read()
+    if img is None: 
+        print('Completed') # At the end of video, print completed and break code
+        break
+    img_reg = cv2.bitwise_and(img, mask)
+
+    results = model(img_reg, stream=True)
+    detections = np.empty((0,5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+            w,h = x2-x1, y2-y1
+
+            # Classname
+            cls = int(box.cls[0])
+
+            # Confodence score
+            conf = math.ceil(box.conf[0]*100)/100
+            
+            if conf > 0.6:
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (x2,y2), scale=1, thickness=1)
+                # cvzone.cornerRect(img, (x1,y1,w,h), l=9)
+                currentArray = np.array([x1,y1,x2,y2,conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultTracker = tracker.update(detections)
+
+    cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 0, 255), 5)
+    cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 0, 255), 5)   
+
+    for res in resultTracker:
+        x1,y1,x2,y2,id = res
+        x1,y1,x2,y2, id = int(x1), int(y1), int(x2), int(y2), int(id)
+        w,h = x2-x1, y2-y1
+
+        cvzone.putTextRect(img, f'{id}', (x1,y1), scale=1, thickness=1, colorR=(0,0,255))
+        cvzone.cornerRect(img, (x1,y1,w,h), l=9, rt=1, colorR=(255,0,255))
+
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        if limitsUp[0] - 15 < cx < limitsUp[2] + 15 and limitsUp[1] < cy < limitsUp[3]:
+            if totalCountUp.count(id) == 0:
+                totalCountUp.append(id)
+                cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 255, 0), 5)
+
+        if limitsDown[0] < cx < limitsDown[2] and limitsDown[3] < cy < limitsDown[1]:
+            if totalCountDown.count(id) == 0:
+                totalCountDown.append(id)
+                cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 255, 0), 5)
+    
+    cv2.putText(img,str(len(totalCountUp)),(929,345),cv2.FONT_HERSHEY_PLAIN,5,(139,195,75),7)
+    cv2.putText(img,str(len(totalCountDown)),(1191,345),cv2.FONT_HERSHEY_PLAIN,5,(50,50,230),7)
+
+    cv2.imshow('Image', img)
+    #cv2.imshow('ImgRead', img_reg)
+    if cv2.waitKey(1) == ord('q'):
+        break
